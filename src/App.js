84f006eac03c2690ad5c49a61f205ba681002963
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-07-16 07:15:54 +0530

    Random Greet

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { getGreet } from './redux/greets/greetSlice';
+import Greetings from './components/Greetings';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(()=> {
-    dispatch(getGreet);
+    dispatch(getGreet());
   }, [dispatch])
   return (
     <div className="App">
       <>hi how are you</>
+      <Greetings />
     </div>
   );
 }
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
 const Greetings = () => {
+  const { greet, isLoading } = useSelector((store) => store.greet)
+  console.log(greet, isLoading)
   return (
     <div>
-      
+      {greet.massage}
     </div>
   )
 }
